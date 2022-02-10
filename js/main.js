8701faf0a183e19b1beffc27888dd4b0268c93dc
stonyDeveloper <stoneachi@gmail.com>
2022-02-10 14:44:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
 const responsive = {
-    0:{
+    0: {
         items: 1
     },
-    320:{
+    320: {
         items: 1
     },
-    560:{
+    560: {
         items: 2
     },
-    960:{
+    960: {
         items: 3
     }
 
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
-   
+
 
     /**click event on toggle menu **/
-    $toggleCollapse.click(function(){
-       $nav.toggleClass('collapse'); 
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse');
     })
-    
+
 
 
     /*-------Owl Carousel for blog-----*/
     $('.owl-carousel').owlCarousel({
         loop: true,
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
         responsive: responsive
     });
 
 
     //click to scroll up
-    $('.move-up span').click(function(){
+    $('.move-up span').click(function() {
         $('html,body').animate({
-            scrollTop:0
+            scrollTop: 0
         }, 2000);
     })
 
